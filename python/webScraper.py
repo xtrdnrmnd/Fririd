36c3c3ef0db0d82bcb41d1956b1cd78e7adf4f6b
xtrdnrmnd <polinkakub@mail.ru>
2022-05-22 18:38:51 +0300

    Wrote some stuff for the parcer

--- python/webScraper.py
+++ python/webScraper.py
@@ -3,6 +3,7 @@ import pandas as pd
 # For parcing the data
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 def ScrapTheList(url):
@@ -10,14 +11,14 @@ def ScrapTheList(url):
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 \
                 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"
     }
-    table_class = "wikitable sortable jquery-tablesorter"
+    table_class = 'wikitable sortable'
 
     response = requests.get(
         url=url,
     )
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    table = soup.find('table', {'class': 'wikitable sortable'}).tbody
+    table = soup.find('table', {'class': table_class}).tbody
     rows = table.find_all('tr')
     columns = [v.text.replace('\n', '') for v in rows[0].find_all('th')]
 
@@ -26,29 +27,16 @@ def ScrapTheList(url):
         tds = rows[i].find_all('td')
         id = 0
         if len(tds) == 4:
-            values = [id, tds[0].text, tds[1].text, tds[2].text,
+            values = [id, tds[0].text.replace('\n', ''), re.sub(r'^.*,', ',', tds[1].text), tds[2].text,
                       tds[3].text]
+            id += 1
         else:
             values = [td.text.replace('\n', ''.replace('\xa0', ''))
                       for td in tds]
-        id += 1
         df = df.append(pd.Series(values, index=columns), ignore_index=True)
     # print(df)
 
-    df.to_csv(r'report.csv', index=False)
+    df.to_csv(r'data/earthquakes.csv', index=False)
 
 
 ScrapTheList("https://en.wikipedia.org/wiki/Lists_of_earthquakes")
-
-# list of name, degree, score
-nme = ["aparna", "pankaj", "sudhir", "Geeku"]
-deg = ["MBA", "BCA", "M.Tech", "MBA"]
-scr = [90, 40, 80, 98]
-
-# dictionary of lists
-dict = {'name': nme, 'degree': deg, 'score': scr}
-
-df = pd.DataFrame(dict)
-
-# saving the dataframe
-df.to_csv('data/earthquakes.csv')
