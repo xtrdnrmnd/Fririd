0950255d4f59d674445db73c2b7013576c075e52
xtrdnrmnd <polinkakub@mail.ru>
2022-05-23 19:35:39 +0300

    visa algo

--- python/DataMining.py
+++ python/DataMining.py
@@ -20,11 +20,13 @@ VMAX = 4
 # Function to calculate the status
 def status(f, t):
     print("kek")
+    return 3
 
 
 # Function to calculate the safety
 def safety(f, t):
-    crimeRateData = open('../data/crimeIndex.csv', 'r')
+    crimeRateData = open('./data/crimeIndex.csv', 'r')
+    return 5
 
 
 # Function to get the visa information
@@ -37,55 +39,89 @@ def visa(fromC, toC):
                 if toC in row[1].replace(" ", ""):
                     cStatus = row[2]
 
-        return (cStatus)
+        print(cStatus)
+    if (cStatus == "Visa not required"):
+        print(4)
+    elif (cStatus == "Visa on arrival"):
+        print(3)
+    elif (cStatus == "Visa required"):
+        print(1)
+    else:
+        print(2)
 
 
 def expenses(fromC, toC):
     # Finding average salary and average salary of "from" Country
     with open('./data/Salary_avg.csv', 'r') as f:
         mycsv = csv.reader(f)
-        allAvgSal = 0
-        numSalRows = 0
-        salary = ""
+        salaryFrom = ""
+        salaryTo = ""
         for row in mycsv:
             if row[0] == fromC:
-                salary += row[1]
-            if row[1] != "Amount($)":
-                row = row[1].replace(",", "")
-                numSalRows += 1
-                allAvgSal += float(row)
+                salaryFrom += row[1]
+            if row[0] == toC:
+                salaryTo += row[1]
 
     with open('./data/csvData.csv', 'r') as f2:
         mycsv2 = csv.reader(f2)
-        CostOfLivingplRent = 0
-        partCost = ""
-        numSalRows2 = 0
+        costFrom = ""
+        costTo = ""
         for row in mycsv2:
             if row[1] == fromC:
-                partCost += row[2]
-            if row[4] != "costLivingPlusRentIndex":
-                numSalRows2 += 1
-                CostOfLivingplRent += float(row[4])
+                costFrom += row[2]
+            if row[1] == toC:
+                costTo += row[2]
 
-    if ():
-        return 4
-    elif ():
-        return 3
-    elif ():
-        return 2
-    else:
-        return 1
+    salaryTo = float(salaryTo.replace(",", ""))
+    salaryFrom = float(salaryFrom.replace(",", ""))
+    costTo = float(costTo)
+    costFrom = float(costFrom)
+    if (salaryFrom > salaryTo + 800):
+        if (costFrom > costTo - 30):
+            print("10 " + str(salaryTo) + " " + str(salaryFrom) +
+                  "    " + str(costFrom) + " " + str(costTo))
+        else:
+            print("9 " + str(salaryTo) + " " + str(salaryFrom) +
+                  "    " + str(costFrom) + " " + str(costTo))
+    elif (salaryFrom > salaryTo + 400 and salaryFrom <= salaryTo + 800):
+        if (costFrom > costTo - 30):
+            print("8 " + str(salaryTo) + " " + str(salaryFrom) +
+                  "    " + str(costFrom) + " " + str(costTo))
+        else:
+            print("7 " + str(salaryTo) + " " + str(salaryFrom) +
+                  "    " + str(costFrom) + " " + str(costTo))
+    elif (salaryFrom <= salaryTo + 400 and salaryFrom >= salaryTo - 400):
+        if (costFrom > costTo - 30):
+            print("6 " + str(salaryTo) + " " + str(salaryFrom) +
+                  "    " + str(costFrom) + " " + str(costTo))
+        else:
+            print("5 " + str(salaryTo) + " " + str(salaryFrom) +
+                  "    " + str(costFrom) + " " + str(costTo))
+    elif (salaryFrom < salaryTo - 400 and salaryFrom >= salaryTo - 800):
+        if (costFrom > costTo - 30):
+            print("4 " + str(salaryTo) + " " + str(salaryFrom) +
+                  "    " + str(costFrom) + " " + str(costTo))
+        else:
+            print("3 " + str(salaryTo) + " " + str(salaryFrom) +
+                  "    " + str(costFrom) + " " + str(costTo))
+    else:  # salaryto < salaryfrom - 800
+        if (costFrom > costTo - 30):
+            print("2 " + str(salaryTo) + " " + str(salaryFrom) +
+                  "    " + str(costFrom) + " " + str(costTo))
+        else:
+            print("1 " + str(salaryTo) + " " + str(salaryFrom) +
+                  "    " + str(costFrom) + " " + str(costTo))
 
 # Algorithm to calculate the rating
 
 
 def count(S, V, B, P):
     R = S * 5 / SMAX + V * 2 / VMAX + B * 2 / BMAX + P / PMAX
-    return R
+    print(R)
 
 
 def main(fromC, toC):
     return count(status(fromC, toC), visa(fromC, toC), safety(fromC, toC), expenses(fromC, toC))
 
 
-visa("Egypt", "Russia")
+visa("Austria", "Egypt")
